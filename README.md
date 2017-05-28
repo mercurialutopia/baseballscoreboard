9f3536e2e9ed65cacaff28cdb0fc961a1ef4717c
Steve Pilling <mercurialutopia@gmail.com>
2017-05-28 14:07:44 -0500

    Preparing upload to GitHub. Adding comments, and a readme.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Baseball Scoreboard
+
+This is a digital replacement for a baseball scoreboard. It allows a scorekeeper to update the various common settings. The current score, etc can be viewed by multiple web users. Updates are pushed live from the Scorekeeper to all Viewers.
+
+
+## Install
+
+Install [Node.js](https://nodejs.org/en/)
+
+Open Console to project folder and run:
+
+```
+npm install
+```
+
+## Testing
+To run the app, open Console to project folder and run:
+
+```
+node app.js
+```
+
+The app URL will be displayed in the Console. To be a viewer only, open the URL as displayed. To keep score, add `/keeper` to the URL.
--- www/app.js
+++ www/app.js
@@ -1,8 +1,8 @@
 angular.module('scoreBoardApp', [])
   .controller('ScoreBoardController', ['socket', function(socket) {
-    var scoreBoard = this;
+	var scoreBoard = this;
 	scoreBoard.homeTeamName = "Home";
-    scoreBoard.homeScore = 0;
+  scoreBoard.homeScore = 0;
 	scoreBoard.visitorTeamName = "Visitor";
 	scoreBoard.visitorScore = 0;
 	scoreBoard.inning = 1;
@@ -21,13 +21,16 @@ angular.module('scoreBoardApp', [])
 	scoreBoard.runnerOnSecond = false;
 	scoreBoard.runnerOnThird = false;
 
-	setInterval(function() { jQuery.ajax(window.location.href); }, 1200000); // 1200000 ms = 20 minutes, This is used for server keep alive. Messages over web sockets don't appear to count. Hopefully it works.
+	// This is used for server keep alive. Messages over web sockets don't appear to count as traffic in heroku so it was hitting the sleep timer on the free instance and resetting the scoreboard.
+	setInterval(function() { jQuery.ajax(window.location.href); }, 1200000); // 1200000 ms = 20 minutes
 
+  // On mobile, auto close the menu after making a selection.
 	$('.nav a').on('click', function(){
     $('.btn-navbar').click(); //bootstrap 2.x
     $('.navbar-toggle').click() //bootstrap 3.x by Richard
 	});
 	
+	// This stores a backup copy of all of the settings on the board. For use with undo.
 	function snapshotState() {
 		// generally history is always saved. This flag is used primarily when starting a new game.
 		if(scoreBoard.historyOn) {
@@ -52,6 +55,7 @@ angular.module('scoreBoardApp', [])
 		}
 	}
 	
+	// This is used by the UI to perform the Undo action. This pulls the last saved state and updates the board to those values.
 	scoreBoard.revertState = function() {
 		if(scoreBoard.history.length > 0) { // if there is a previous state
 			//pull the previous state and set all values
@@ -72,7 +76,8 @@ angular.module('scoreBoardApp', [])
 		}
 	}
  
-    function setBSO(strikes, balls, outs) {
+	// All changes to strikes, balls and outs are related. When any changes need to be made to these fields, use this method. 
+ 	function setBSO(strikes, balls, outs) {
 		var changeInning = false;
 		if(balls == 4) { // if there are 4 balls then the player has been walked, reset the count
 			balls = 0;
@@ -107,28 +112,39 @@ angular.module('scoreBoardApp', [])
 			socket.emit('boardUpdate', update);
 		}
 	}
-    scoreBoard.addStrike = function() {
+
+	//used by the UI when adding a strike.
+	scoreBoard.addStrike = function() {
 		setBSO(1+scoreBoard.strikes, scoreBoard.balls, scoreBoard.outs);
-    };
-    scoreBoard.removeStrike = function() {
+	};
+	//used by the UI when removing a strike.
+	scoreBoard.removeStrike = function() {
 		setBSO(scoreBoard.strikes-1, scoreBoard.balls, scoreBoard.outs);
-    };
-    scoreBoard.addBall = function() {
+	};
+	//used by the UI when adding a ball.
+	scoreBoard.addBall = function() {
 		setBSO(scoreBoard.strikes, 1+scoreBoard.balls, scoreBoard.outs);
-    };
-    scoreBoard.removeBall = function() {
+	};
+	//used by the UI when removing a ball.
+	scoreBoard.removeBall = function() {
 		setBSO(scoreBoard.strikes, scoreBoard.balls-1, scoreBoard.outs);
-    };
-    scoreBoard.addOut = function() {
+	};
+	//used by the UI when adding an out.
+	scoreBoard.addOut = function() {
 		setBSO(scoreBoard.strikes, scoreBoard.balls, 1+scoreBoard.outs);
-    };
-    scoreBoard.removeOut = function() {
+	};
+	//used by the UI when removing an out.
+	scoreBoard.removeOut = function() {
 		setBSO(scoreBoard.strikes, scoreBoard.balls, scoreBoard.outs-1);
-    };
-    scoreBoard.newBatter = function() {
-		// a new batter clears the count, but does not effect the outs
+	};
+	// Used to clear the count.
+	scoreBoard.newBatter = function() {
+	// a new batter clears the count, but does not effect the outs
 		setBSO(0, 0, scoreBoard.outs);
-    };
+	};
+
+
+	// Updates the home score.
 	function setHomeScore(score) {
 		if(score >= 0) { // teams may not have a score below zero
 			snapshotState();
@@ -137,12 +153,17 @@ angular.module('scoreBoardApp', [])
 			socket.emit('boardUpdate', update);
 		}
 	}
+	// Used by the UI when adding a run for the home team.
 	scoreBoard.addRunHome = function() {
 		setHomeScore(1+scoreBoard.homeScore);
 	};
+	// Used by the UI when removing a run for the home team.
 	scoreBoard.removeRunHome = function() {
 		setHomeScore(scoreBoard.homeScore-1);
 	};
+
+
+	// Updates the home score.
 	function setVisitorScore(score) {
 		if(score >= 0) { // teams may not have a score below zero
 			snapshotState();
@@ -151,12 +172,17 @@ angular.module('scoreBoardApp', [])
 			socket.emit('boardUpdate', update);
 		}
 	}
+	// Used by the UI when adding a run for the visitor team.
 	scoreBoard.addRunVisitor = function() {
 		setVisitorScore(1+scoreBoard.visitorScore);
 	};
+  // Used by the UI when removing a run for the visitor team.
 	scoreBoard.removeRunVisitor = function() {
 		setVisitorScore(scoreBoard.visitorScore-1);
 	};
+
+
+  // Updates the inning
 	function setInning(inning, isTop) {
 		if(inning > 0) { // the inning must be at least the first (1)
 			snapshotState();
@@ -170,6 +196,7 @@ angular.module('scoreBoardApp', [])
 			socket.emit('boardUpdate', update);
 		}
 	}
+	// Used by the UI when advancing the inning
 	scoreBoard.nextHalfInning = function() {
 		// if it is currently the top of the inning then it will become the bottom of the same inning
 		if(scoreBoard.isTopOfInning) {
@@ -178,6 +205,7 @@ angular.module('scoreBoardApp', [])
 			setInning(1+scoreBoard.inning, true);
 		}
 	}
+	// Used by the UI when correcting the inning
 	scoreBoard.previousHalfInning = function() {
 		//if it is currently the top of the inning then it will become the bottom of the previous inning
 		if(scoreBoard.isTopOfInning) {
@@ -186,6 +214,9 @@ angular.module('scoreBoardApp', [])
 			setInning(scoreBoard.inning, true);
 		}
 	};
+
+
+	// Used by the UI when toggling the runner on base state. Called with 1, 2, or 3. 
 	scoreBoard.runnerOn = function(base) {
 		switch(base) {
 			case 1:
@@ -205,15 +236,23 @@ angular.module('scoreBoardApp', [])
 				break;
 		}
 	};
+
+
+	// Turns the correction mode on and off.
 	function setCorrectionMode(correctionModeOn) {
 		scoreBoard.correctionModeOn = correctionModeOn;
 	}
+	// Used by the UI to toggle the correction mode.
 	scoreBoard.toggleCorrectionMode = function() {
 		setCorrectionMode(!scoreBoard.correctionModeOn);
 	};
+	// Used by the UI to see if correction mode is on.
 	scoreBoard.isCorrectionMode = function() {
 		return scoreBoard.correctionModeOn;
 	};
+
+
+  // Used by the UI to clears the board in preparation for a new game.
 	scoreBoard.newGame = function() {
 		var confirmNewGame = confirm("Are you sure you want to clear the current game?");
 		if(confirmNewGame) {
@@ -245,13 +284,17 @@ angular.module('scoreBoardApp', [])
 		}
 	};
 	
+
+	// Used by the UI when starting the edit of the name of the Teams.
 	scoreBoard.editNames = function() {
 		scoreBoard.nameEdit.homeTeamName = scoreBoard.homeTeamName;
 		scoreBoard.nameEdit.visitorTeamName = scoreBoard.visitorTeamName;
 	};
+	// Used by the UI when saving the new team names.
 	scoreBoard.saveNames = function() {
 		saveTeamNames(scoreBoard.nameEdit.homeTeamName, scoreBoard.nameEdit.visitorTeamName);
 	};
+	// Changes the team names.
 	function saveTeamNames(homeTeamName, visitorTeamName) {
 		scoreBoard.homeTeamName = homeTeamName;
 		scoreBoard.visitorTeamName = visitorTeamName;
@@ -259,6 +302,8 @@ angular.module('scoreBoardApp', [])
 		socket.emit('boardUpdate', update);
 	}
 	
+
+	// Used on page load. This listens for a message from the server to set all board settings. 
 	socket.on('init', function (data) {
 		var keys = Object.keys(data);
 		for(var i = 0; i < keys.length; i++) {
@@ -266,9 +311,11 @@ angular.module('scoreBoardApp', [])
 		}
 	});
 	
-  }])
+  }]) // end of the controller class
+	
+	// Magic. Honestly I can't remember what this does.
   .factory('socket', function ($rootScope) {
-  var socket = io.connect();
+  var socket = io.connect(); // connect to the server
   return {
     on: function (eventName, callback) {
       socket.on(eventName, function () {  
